f0043185f530dc2e50eee63c36376ac019f21be9
Sahand <60545395+Sahand-Sarhangi@users.noreply.github.com>
2024-07-23 08:32:37 +0330

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -13,7 +13,7 @@ const CONFIG = {
   projects: {
     github: {
       display: true, // Display GitHub projects?
-      header: 'صفحه شخصی من',
+      header: 'نمونه کدهای منبع باز من',
       mode: 'automatic', // Mode can be: 'automatic' or 'manual'
       automatic: {
         sortBy: 'stars', // Sort projects by 'stars' or 'updated'
